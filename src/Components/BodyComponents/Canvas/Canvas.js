d3ef04c21eef8c1b5e914a724a281e12e9191f5d
Arielle Ross-Levy <ariellerosslevy@Arielles-MacBook-Air.local>
2021-03-19 15:53:32 -0600

    can now post linecoordinates for drawings

--- src/Components/BodyComponents/Canvas/Canvas.js
+++ src/Components/BodyComponents/Canvas/Canvas.js
@@ -12,23 +12,38 @@ export default function Canvas() {
         width: 800,
         height: 500,
         brushRadius: 5,
-        lazyRadius: 0
+        lazyRadius: 0,
+        imgSrc: "https://images.unsplash.com/flagged/photo-1556438758-872c68902f60?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=753&q=80"
       })
 
       const canvas = useRef();
+      const testImage = "https://i1.wp.com/www.southwestconferenceblog.org/wp-content/uploads/2018/05/this-is-a-test-wp.png?fit=825%2C510"
+
+      const saveDrawingToDatabase = (projectName, lineCoordinates, image) => {
+        fetch('http://localhost:4000/drawings', {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json" 
+            },
+            body: JSON.stringify(
+                { projectName, lineCoordinates, image }
+            )
+        })
+      }
 
     return (
         <div>
           <div className="canvas-attributes">
             <button
                 onClick={() => {
-                localStorage.setItem(
-                    "savedDrawing",
-                    canvas.current.getSaveData()
-                );
+                    localStorage.setItem(
+                        "savedDrawing",
+                        canvas.current.getSaveData()
+                    )
+                    saveDrawingToDatabase("TEST project", localStorage.getItem("savedDrawing"), testImage)
                 }}
             >
-                Save
+            Save
             </button>
 
             <button
@@ -86,7 +101,8 @@ export default function Canvas() {
           lazyRadius={state.lazyRadius}
           canvasWidth={state.width}
           canvasHeight={state.height}
-          imgSrc= {"https://images.unsplash.com/flagged/photo-1556438758-872c68902f60?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=753&q=80"}
+          imgSrc= {state.imgSrc}
+          loadTimeOffset = {0}
 
         />
             
