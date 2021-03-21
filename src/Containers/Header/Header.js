bc5b2f1b53a96f1211fe2f4d14437bb05246f081
Arielle Ross-Levy <ariellerosslevy@Arielles-MacBook-Air.local>
2021-03-21 14:32:01 -0600

    created project show page

--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -8,6 +8,7 @@ import './Header.css'
 export default function Header() {
     return (
         <div className="header-container">
+            <div className="header__bg"></div>
             <HeaderNavigation />
             <HeaderLogin />
         </div>
