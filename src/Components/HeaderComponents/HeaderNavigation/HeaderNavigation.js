4cab94e6379f889a88abccb3d745ce706a1fe695
Arielle Ross-Levy <ariellerosslevy@arielles-air.lan>
2021-03-29 12:31:11 -0400

    adding new project page link in nav, removed link from header logo, and added login page

--- src/Components/HeaderComponents/HeaderNavigation/HeaderNavigation.js
+++ src/Components/HeaderComponents/HeaderNavigation/HeaderNavigation.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Link, Switch, Redirect } from 'react-router-dom'
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 import logo from '../../../Assets/mayk-white-logo.png'
 
@@ -8,8 +9,9 @@ import './HeaderNavigation.css'
 export default function HeaderNavigation() {
     return (
         <div className="header-navigation">
-            <Link to="/"><img className="logo" src={logo} alt="mayk logo" /></Link>
+            <img className="logo" src={logo} alt="mayk logo" />
             <h3 className="nav-list">
+            <Link className="new-project" to="/NewProject"><AddCircleIcon className="circle-icon" fontSize="large"/></Link>
             <Link className="projects" to="/Projects">Projects</Link> |
             Lending Library |
             Account
