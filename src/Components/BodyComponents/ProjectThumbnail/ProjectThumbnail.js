8a5864b7968567daac8923f25ea44eb52a613fa2
Arielle Ross-Levy <ariellerosslevy@arielles-air.lan>
2021-04-01 13:05:55 -0400

    ability save drawings, reopen canvas from projects page, and reopen line coordinates

--- src/Components/BodyComponents/ProjectThumbnail/ProjectThumbnail.js
+++ src/Components/BodyComponents/ProjectThumbnail/ProjectThumbnail.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Canvas from '../Canvas/Canvas'
 
 import './ProjectThumbnail.css'
 
 export default function ProjectThumbnail( { project }) {
 
-    const { projectName, image } = project
+    const { id, projectName, image, lineCoordinates } = project
+
+    const [openCanvas, setOpenCanvas] = useState(false)
+
+    const thumbnailSelected = () => {
+        setOpenCanvas(true)
+    }
 
     return (
-        <div className="project-thumbnail">
-            <h2>{ projectName }</h2>
-            <img className='thumbnail-image' src={ image } alt={projectName}></img>
+        <div>
+            {openCanvas
+                ? <Canvas id={ id } projectName={ projectName } imageProp={ image } lineCoordinates = { lineCoordinates }/>
+                : <div className="project-thumbnail" onClick={ thumbnailSelected }>
+                    <h2>{ projectName }</h2>
+                    <img className='thumbnail-image' src={ image } alt={projectName}></img>
+                </div>
+            }
+            
         </div>
     )
 }
